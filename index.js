cf8c3369ebd8d08ba4b1a7f17d93565620fcca7b
LESANF <nagong1000@naver.com>
2020-03-23 18:49:54 +0900

    ING

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import express from "express";
+import express, { request } from "express";
 import axios from "axios";
 import pug from "pug";
 import bodyParser from "body-parser";
 import session from "express-session";
+//import request from "request";
 
 const app = express();
 
@@ -10,15 +11,15 @@ app.set("view engine", "pug");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", async (req, res) => {
-  try {
-    const { data: geoLocation } = await axios.get("http://ip-api.com/json/");
-    console.log("I received information !");
-    res.render("index.pug", { lesa: geoLocation, page: "hi" });
-  } catch (error) {
-    console.log(" E R R O R!");
-  }
-  //   res.json(geoLocation);
+const url = "https://api.coinpaprika.com/v1/tickers";
+
+app.get("/", (req, res) => {
+  request.get(url, function(error, response, html) {
+    console.log(html);
+    // const obj = JSON.parse(html);
+    // console.log(obj[1]);
+  });
+  // res.render("index.pug", { coin });
 });
 
 const handleStart = () => {
